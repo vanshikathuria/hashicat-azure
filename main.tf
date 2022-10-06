bc52120d9654531487433dd72a41aba22a1bfbec
vanshi kathuria <vanshi.kathuria@morganstanley.com>
2022-10-06 15:48:33 +0000

    added the first tag

--- main.tf
+++ main.tf
@@ -104,6 +104,7 @@ resource "azurerm_public_ip" "catapp-pip" {
 
 resource "azurerm_virtual_machine" "catapp" {
   name                = "${var.prefix}-meow"
+  department = "devops"
   location            = var.location
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
